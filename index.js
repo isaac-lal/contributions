fb18478964299ef68e8932e2bac90e8eb7481238
isaaclal124 <isaaclal124@gmail.com>
2022-12-21 12:14:45 -0500

    OK

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
-
+const random = require('random');
 const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const makeCommit = n => {
+    if(n == 0) return simpleGit().push();
 
-const data = {
-    date: DATE
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+
+    const data = {
+        date: DATE
+    }
+
+    console.log(DATE);
+
+    jsonfile.writeFile(FILE_PATH, data); 
+    
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, makeCommit.bind(this, --n));
 }
 
-jsonfile.writeFile(FILE_PATH, data);
-
-// git commit --date = "" to modify the date previously
-
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }).push();
+makeCommit(100);
