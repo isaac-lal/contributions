b26c743fb6b20e546827142b48c32e1a3ae4a3d4
Isaac Lal <isaaclal124@gmail.com>
2023-02-07 00:00:00 -0500

    commit

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
-from random import randint
+import random
+from datetime import datetime, timedelta
 
-for i in range(1, 5):
-    for j in range(0, randint(1, 3)):
-        d = str(i) + " days ago"
+# Define start and end dates
+start_date = datetime(2023, 2, 7)
+end_date = datetime(2024, 5, 15)
 
+# Generate all dates in the range
+delta = (end_date - start_date).days
+dates = [start_date + timedelta(days=i) for i in range(delta + 1)]
+
+# Loop over each date
+for date in dates:
+    for _ in range(random.randint(1, 3)):  # 1 to 3 commits per day
+        d_str = date.strftime("%Y-%m-%d %H:%M:%S")
+        
         with open("file.txt", "a") as file:
-            file.write(d)
+            file.write(d_str + "\n")
         
         os.system("git add .")
-        os.system('git commit --date="' + d + '" -m "commit"')
+        os.system(f'git commit --date="{d_str}" -m "commit"')
 
 os.system('git push -u origin main')
